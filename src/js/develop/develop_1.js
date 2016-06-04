3b792b812812b4f0a36d1cdb9a08bf336d2c5cfc
Dmitry <dmitry1939@gmail.com>
2016-06-04 13:01:32 +0300

    block-1 desktop

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,7 +1,22 @@
+/* block-1 slider */
 
+    function blockOneSlider(){
+
+        $('.block-1-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 4000,
+            fade: true,
+            cssEase: 'linear'
+        });
+
+    }
+
+/* /block-1-slider */
 
 
 $(document).ready(function(){
+
+    blockOneSlider();
 
 });
 
